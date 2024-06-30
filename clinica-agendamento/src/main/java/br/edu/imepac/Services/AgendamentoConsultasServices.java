571160d2834a0227428f043f889d728783ffadb5
ThaynaraxX <silvathaynara428@gmail.com>
2024-06-30 13:00:53 -0300

    Revendo atualizações

--- clinica-agendamento/src/main/java/br/edu/imepac/Services/AgendamentoConsultasServices.java
+++ clinica-agendamento/src/main/java/br/edu/imepac/Services/AgendamentoConsultasServices.java
@@ -4,12 +4,15 @@ import br.edu.imepac.Model.AgendamentoConsultasModel;
 import br.edu.imepac.Repositories.AgendamentoConsultasRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
 @Service
-public class AgendamentoConsultasServices {@Autowired
-private AgendamentoConsultasRepositories repository;
+public class AgendamentoConsultasServices {
+    @Autowired
+    private AgendamentoConsultasRepositories repository;
 
     public List<AgendamentoConsultasModel> findAll() {
         return repository.findAll();
@@ -21,6 +24,14 @@ private AgendamentoConsultasRepositories repository;
 
     public void deleteById(int id) {
         repository.deleteById((long) id);
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(AgendamentoConsultasServices.class);
+
+    public void agendarConsulta() {
+        logger.info("Agendamento de consulta iniciado.");
+        logger.info("Agendamento de consulta concluído.");
+
     }
 }
 
